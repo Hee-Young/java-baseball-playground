e71d690d2f738f0eadd1cdfc2943cc6d20186f9c
Hee-Young <kkhy991222@gmail.com>
2024-05-28 21:55:27 +0900

    feat: string 클래스 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,38 @@
 package study;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
     void replace() {
-        String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");
+        assertThat("abc".replace("b", "d")).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        assertThat("1,2".split(",")).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt() {
+        assertThat("abc".charAt(0)).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("charAt() 은 문자열의 길이보다 큰 인수를 받을 수 없다.")
+    void exception() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(5);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+          .hasMessageContaining("String index out of range: 5");
     }
 }
