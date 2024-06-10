b86c8867983f8b577797d80b097be11509418d1c
Hee-Young <kkhy991222@gmail.com>
2024-06-10 23:14:47 +0900

    refactor: 문자열 계산기

--- src/test/java/string/CalculatorTest.java
+++ src/test/java/string/CalculatorTest.java
@@ -2,65 +2,59 @@ package string;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
     Calculator calculator = new Calculator();
 
-    public static void inputHandling(String userInput) {
-        InputStream in = new ByteArrayInputStream(userInput.getBytes());
-        System.setIn(in);
-    }
-
-    @Test
-    void example(){
-        inputHandling("2 + 3 * 4 / 2");
-        assertThat(calculator.input()).isEqualTo(10);
+    @ParameterizedTest
+    @CsvSource(value = {
+            "2 + 3 * 4 / 2:10",
+            "5 * 2 + 9:19",
+            "2 + 6 / 2 * 11:44",
+            "4 - 3 + 8 * 10:90",
+            "10 / 5 + 2 / 4:1",
+            "0 - 2:-2"
+    }, delimiter = ':')
+    void normalCase(String input, int expected) {
+        assertEquals(calculator.calculate(input), expected);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {
-            "5 * 2 + 9",
-            "2 + 6 / 2 * 11",
-            "4 - 3 + 8 * 10",
-            "10 / 5 + 2 / 4"
-    })
-    void normalCase(String str){
-        inputHandling(str);
-        assertThat(calculator.input()).isNotEqualTo(-1);
-    }
-
-    @Test
-    void strangeCase(){
-        inputHandling("// 1 /2+");
-        assertThat(calculator.input()).isEqualTo(0);
-    }
-    @ParameterizedTest
-    @ValueSource(strings = {
-            "// 1 /2+",
-            "2/1+4",
             "+ 8 / 4 * 21",
             "1 +5 + 2 / 4"
     })
-    void strangeCase(String str){
-        inputHandling(str);
-        assertThat(calculator.input()).isNotEqualTo(-1);
+    void strangeOperandsException(String input) {
+        assertThatThrownBy(() -> {
+            calculator.calculate(input);
+        }).isInstanceOf(NumberFormatException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "2/1+4",
+            "",
+            "  ",
+            "1 h 2"
+    })
+    void strangeInputException(String input){
+        assertThatThrownBy(() -> {
+            calculator.calculate(input);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void divideZero() {
-        inputHandling("2 + 1 / 0 + 4");
-        assertThat(calculator.input()).isEqualTo(-1); // 뒤에 + 4 때문에 0이 아니라 4,,
-    }
-
-    @Test
-    void strangeOperator() {
-        inputHandling("2 c 1 / 3 + 4");
-        assertThat(calculator.input()).isEqualTo(-1); // 뒤에 + 4 때문에 0이 아니라 4,,
+        assertThatThrownBy(() -> {
+            calculator.calculate("2 + 1 / 0 + 4");
+        }).isInstanceOf(ArithmeticException.class);
     }
 }
